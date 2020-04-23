6a1924776e8b3bc66f36fd73e0e6f296fb1bd5b5
Baruch Odem <baruchiro@gmail.com>
2020-04-23 19:32:42 +0300

    Get values from Env or args

--- telegram-download-daemon.py
+++ telegram-download-daemon.py
@@ -3,6 +3,7 @@
 # Author: Alfonso E.M. <alfonso@el-magnifico.org>
 # You need to install telethon (and cryptg to speed up downloads)
 
+from os import getenv
 
 from telethon import TelegramClient, events
 from telethon.tl.types import PeerChannel
@@ -10,12 +11,26 @@ import logging
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s]%(name)s:%(message)s',level=logging.WARNING)
 
+import argparse
+
+TELEGRAM_DEAMON_API_ID = getenv("TELEGRAM_DEAMON_API_ID")
+TELEGRAM_DEAMON_API_HASH = getenv("TELEGRAM_DEAMON_API_HASH")
+TELEGRAM_DEAMON_CHANNEL = getenv("TELEGRAM_DEAMON_CHANNEL")
+
+parser = argparse.ArgumentParser(description="Script to download files from Telegram Channel.")
+parser.add_argument("--api-id", required=TELEGRAM_DEAMON_API_ID == None, type=str, default=TELEGRAM_DEAMON_API_ID, help='api_id from https://core.telegram.org/api/obtaining_api_id (default is TELEGRAM_DEAMON_API_ID env var)')
+parser.add_argument("--api-hash", required=TELEGRAM_DEAMON_API_HASH == None, type=str, default=TELEGRAM_DEAMON_API_HASH, help='api_hash from https://core.telegram.org/api/obtaining_api_id (default is TELEGRAM_DEAMON_API_HASH env var)')
+parser.add_argument("--dest", type=str, default=getenv("TELEGRAM_DEAMON_DEST", "/telegram-downloads"), help='Destenation path for downloading files (default is /telegram-downloads).')
+parser.add_argument("--channel", required=TELEGRAM_DEAMON_CHANNEL == None, type=str, default=TELEGRAM_DEAMON_CHANNEL, help='Channel id to download from it (default is TELEGRAM_DEAMON_CHANNEL env var')
+args = parser.parse_args()
+
+api_id = args.api_id
+api_hash = args.api_hash
+channel_id = args.channel
+downloadFolder = args.dest
+
 # Edit these lines:
-api_id = NNNNNN
-api_hash = "XXXXXXXXXXXXXXXXXXXXXXXXXX"
-downloadFolder="/tmp"
-channel_id= NNNNNNNNN
-proxy = None  
+proxy = None
 # End of interesting parameters 
 
 session = "DownloadDaemon"
