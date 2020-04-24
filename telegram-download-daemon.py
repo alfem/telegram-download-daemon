63e38aa85614bade36872137ede40e057689e143
Baruch Odem <baruchiro@gmail.com>
2020-04-24 10:17:01 +0300

    Some fixes and refactor
    (cherry picked from commit 0617fc013fba5054e88665fd346caef3aca58702)

--- telegram-download-daemon.py
+++ telegram-download-daemon.py
@@ -24,17 +24,22 @@ client = TelegramClient(session, api_id, api_hash, proxy=proxy).start()
 
 @client.on(events.NewMessage())
 async def handler(event):
+    
+    async def log_respond(respond):
+        print(respond)
+        await event.respond(respond)
+
     if event.to_id != PeerChannel(channel_id):
         return
+    
     print(event)
+    
     if event.media:
        filename=event.media.document.attributes[0].file_name
-       answer="Downloading file %s (%i bytes)" % (filename,event.media.document.size)
-       print(answer)
-       await event.respond(answer)
+       log_respond(f"Downloading file {filename} ({event.media.document.size} bytes)")
 
-       await client.download_media(event.message,downloadFolder)
-       await event.respond(filename+" ready")
+       await client.download_media(event.message, downloadFolder)
+       log_respond(f"{filename} ready")
 
 
 with client:
