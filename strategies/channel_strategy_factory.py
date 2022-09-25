ad4dbf58396a6144386ebc047964650e162a14cb
José Ángel Rey Liñares <jose.angel.rey.linares@gmail.com>
2022-09-25 16:38:26 +0200

    feat: added support for prometeo releases music channel
    added support for prometeo releases music channel

--- strategies/channel_strategy_factory.py
+++ strategies/channel_strategy_factory.py
@@ -6,6 +6,7 @@ from config.daemon_config import DaemonConfig
 from strategies.base import BaseChannelManager
 from strategies.exception.ChannelStrategyNotFoundException import ChannelStrategyNotFoundException
 from strategies.fresh_electronic_music_channel import FreshElectronicMusicChannelStrategy
+from strategies.prometeo_releases_music import PrometeoReleasesMusicChannelStrategy
 from strategies.rave_music_releases_channel import RaveMusicReleasesChannelStrategy
 
 
@@ -15,6 +16,7 @@ class ChannelStrategyFactory(ABC):
             FreshElectronicMusicChannelStrategy(daemon_config)
         self.rave_music_releases_manager: RaveMusicReleasesChannelStrategy = \
             RaveMusicReleasesChannelStrategy(daemon_config)
+        self.prometeo_releases_music_manager : PrometeoReleasesMusicChannelStrategy = PrometeoReleasesMusicChannelStrategy(daemon_config)
 
     def get_channel_manager(self, channel_id: int) -> BaseChannelManager:
         target_strategy: BaseChannelManager
@@ -25,6 +27,9 @@ class ChannelStrategyFactory(ABC):
         elif channel_id in self.rave_music_releases_manager.managed_channel:
             print(f"Manager detected as RMR channel manager.")
             target_strategy = self.rave_music_releases_manager
+        elif channel_id in self.prometeo_releases_music_manager.managed_channel():
+            print(f"Manager detected as PRM channel manager.")
+            target_strategy = self.prometeo_releases_music_manager
         else:
             print(f"Provided channel id not managed by any existing manager.")
             raise ChannelStrategyNotFoundException()
---
+++ strategies/prometeo_releases_music.py
@@ -0,0 +1,33 @@
+import string
+
+from config.daemon_config import DaemonConfig
+from strategies.base import BaseChannelManager
+from sanitize_filename import sanitize
+
+
+class PrometeoReleasesMusicChannelStrategy(BaseChannelManager):
+
+    def __init__(self, daemon_config: DaemonConfig):
+        super().__init__(daemon_config)
+        self._managed_channel: list[int] = [1218961921]
+
+    def extract_folder_name(self, picture_message: string) -> string:
+        print(f"Input message received: {picture_message}")
+        print("Splitted output:")
+        print(picture_message.splitlines())
+        print(f"Resolved folder name: ", picture_message.splitlines()[0])
+        line1: string = picture_message.splitlines()[0]
+        hashtags: list = []
+        index: int = 0
+
+        if line1 is not None:
+            hashtags = [i for i in line1.split("-") if (i.startswith("#") or i.startswith("@"))]
+
+        if len(line1.split()) == len(hashtags):
+            index = 2
+
+        return sanitize(picture_message.splitlines()[index])
+
+    @property
+    def managed_channel(self) -> list[int]:
+        return self._managed_channel
